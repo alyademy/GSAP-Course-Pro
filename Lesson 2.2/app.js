6d839109a0b7f163cf3f7403b38c774366472c6a
Oleksandra Dementych <alyad96@gmail.com>
2025-11-03 14:37:31 -0500

    first commit

---
+++ Lesson 2.2/app.js
@@ -0,0 +1,17 @@
+const titleProject = "Оценка стоимости";
+console.log('Название проекта:', titleProject, typeof titleProject);
+
+const screensValue = "шаблонные, с уникальным дизайном, с анимациями";
+console.log('Типы экранов:', screensValue, typeof screensValue);
+
+const screenPrice = 1000;
+console.log('Цена реализации одного экрана:', screenPrice, typeof screenPrice);
+
+const percentage = 55;
+console.log('Процент отката подрядчику:', percentage, typeof percentage);
+
+const fullPrice = 5000;
+console.log('Желаемая сумма заработка с проекта:', fullPrice, typeof fullPrice);
+
+const responsive = true;
+console.log(responsive, typeof responsive);
